885612613cf1ba38fc831b5a15223452ee87173a
KSroido <ksxmyqj@gmail.com>
2022-07-07 12:51:11 +0800

    done basic markdown css

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -3,12 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import ArirticlePost
+import markdown
+
 def article_list(request):
     articles = ArirticlePost.objects.all()
     context = {'articles': articles}
     return render(request, 'article/list.html', context)
     #return HttpResponse("Hello World!")
+
 def article_details(request, id):
     article = ArirticlePost.objects.get(id=id)
+    article.body = markdown.markdown(
+        article.body,
+        extensions=[
+        'markdown.extensions.extra',
+        # 语法高亮扩展
+        'markdown.extensions.codehilite',
+        ])
     context = {'article': article}
     return render(request, 'article/detail.html', context)
--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("article/", include("article.urls", namespace="article" )),
+    path("article/", include("article.urls", namespace="article")),
+
 ]
