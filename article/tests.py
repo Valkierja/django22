42e3a213da8b9d650fde8609317d9415e4f683bd
KSroido <ksxmyqj@gmail.com>
2022-07-29 22:26:35 +0800

    11

--- article/tests.py
+++ article/tests.py
@@ -11,5 +11,5 @@ import os
 class AnimalTestCase(TestCase):
     def test_animals_can_speak(self):
         """Animals that can speak are correctly identified"""
-        self.assertEqual(my_blog.settings.SECRET_KEY, os.environ["SECRET_KEY"])
-        print("BASE_DIR is: " + my_blog.settings.BASE_DIR)
+
+        print(my_blog.settings.STATIC_URL)
--- my_blog/settings.py
+++ my_blog/settings.py
@@ -152,13 +152,12 @@ USE_TZ = True
 # #
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static/"),
+# )
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
+STATIC_URL = 'static/'
 
-STATIC_URL = '/static/'
-
-# 'static/' is wrong
