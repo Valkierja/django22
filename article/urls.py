327bcd319eceb417cb1c22b3a1d67ed68d20af92
KSroido <ksxmyqj@gmail.com>
2022-07-30 20:11:48 +0800

    set static_url

--- article/urls.py
+++ article/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 
 app_name = 'article'
 urlpatterns = [
-    # path('', lambda request: redirect('article-list/', permanent=True)),
+    path('', lambda request: redirect('article-list/', permanent=True)),
     path('article-list/', views.article_list, name='article_list'),
     path('article-detail/<int:id>/', views.article_details, name='article_detail'),
 ]
