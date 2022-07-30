153f63ad1c2c0cd6e285b39224e66c41d30b6939
KSroido <ksxmyqj@gmail.com>
2022-07-30 19:35:55 +0800

    set static_url

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -156,7 +156,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 #     os.path.join(BASE_DIR, "static/"),
 # )
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = 'static/'
+STATIC_URL = '/static'
 
 # 由于不同情况下和版本下的表现形式的不一致
 # STATIC_URL， STATIC_ROOT， STATICFILES_DIRS这三个变量的左边和邮编是否加斜杠是非常有讲究的，注意查看流量抓包，尝试所有反斜杠存在组合
