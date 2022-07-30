91d5c2f716efce388150b04ea5ec3b97d3bacdd3
KSroido <ksxmyqj@gmail.com>
2022-07-30 19:41:55 +0800

    set static_url

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -16,8 +16,10 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 IS_HEROKU = "DYNO" in os.environ
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
