28de597de96799d034d4de3c74b91deb72c0ec84
KSroido <ksxmyqj@gmail.com>
2022-07-30 20:02:07 +0800

    set static_url

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -153,11 +153,11 @@ USE_TZ = True
 # #
 
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static/"),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = BASE_DIR / "static"
+# STATIC_ROOT = BASE_DIR / "static"
 
 STATIC_URL = 'static/'
 
