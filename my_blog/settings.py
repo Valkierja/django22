8dd15a3d08cf832601d9808bb979ff4bacc37c3a
KSroido <ksxmyqj@gmail.com>
2022-08-18 23:05:03 +0800

    change settings.py to django3 form

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -163,7 +163,7 @@ if IS_HEROKU:
     STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static", "layui"),
 )
 
 # 由于不同情况下和版本下的表现形式的不一致
