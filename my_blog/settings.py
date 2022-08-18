953cddcd1e60353f91a571978e3122f7eb9504bf
KSroido <ksxmyqj@gmail.com>
2022-08-18 22:57:23 +0800

    change settings.py to django3 form

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -158,12 +158,13 @@ USE_TZ = True
 # STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "/static/"
 
+
 if IS_HEROKU:
     STATIC_ROOT = BASE_DIR / "staticfiles"
-else:
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, "static"),
-    )
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 # 由于不同情况下和版本下的表现形式的不一致
 # STATIC_URL， STATIC_ROOT， STATICFILES_DIRS这三个变量的左边和邮编是否加斜杠是非常有讲究的，注意查看流量抓包，尝试所有反斜杠存在组合
