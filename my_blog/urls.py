03dd032ae3c520522e76b4a1c89c27369a118b62
ksroido <ksxmyqj@gmail.com>
2022-09-14 20:52:58 +0800

    1

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -27,7 +27,6 @@ import article
 from article import views
 
 urlpatterns = [
-    path(r'^$', lambda request: redirect('article/article-list/', permanent=True)),
     path('admin/', admin.site.urls),
     path("article/", include("article.urls", namespace="article")),
     path('userprofile/', include('userprofile.urls', namespace='userprofile')),
