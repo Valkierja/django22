73a40af1ce176d28f458f39a2b1307b8e86b8f6c
ksroido <ksxmyqj@gmail.com>
2022-09-14 21:51:00 +0800

    wsgi delete

--- my_blog/wsgi.py
+++ my_blog/wsgi.py
@@ -1,15 +1,15 @@
-"""
-WSGI config for my_blog project.
+# """
+# WSGI config for my_blog project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+# """
 
-import os
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# import os
+# from django.core.wsgi import get_wsgi_application
+# from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_blog.settings')
-application = Cling(get_wsgi_application())
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_blog.settings')
+# application = Cling(get_wsgi_application())
