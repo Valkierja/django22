8b2711ca768afb6fb4c4cca97ee7eab27844c2f8
ksroido <ksxmyqj@gmail.com>
2022-09-15 22:36:53 +0800

    env PASSWD

--- bakUp/backUp.py
+++ bakUp/backUp.py
@@ -2,10 +2,12 @@ from datetime import datetime
 
 from webdav4.client import Client
 import pathlib
-
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+PASSWD = os.environ['PASSWD']
 # TODO:not add webdav4 to requirements.txt yet
 client = Client(base_url='https://dav.jianguoyun.com/dav/bak',
-                auth=('ksxmyqj@gmail.com', ''))  # add passwd
+                auth=('ksxmyqj@gmail.com', PASSWD))  # add passwd
 
 fileName = datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.sqlite3'
 pathlike = pathlib.Path('../db.sqlite3')
