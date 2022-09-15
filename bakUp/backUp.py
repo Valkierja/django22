f9aed9298b7512fb5f96dcbbf3f8a40c9b685fb0
ksroido <ksxmyqj@gmail.com>
2022-09-15 22:39:21 +0800

    backup import os

--- bakUp/backUp.py
+++ bakUp/backUp.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from webdav4.client import Client
 import pathlib
 from dotenv import load_dotenv, find_dotenv
+import os 
+
 load_dotenv(find_dotenv())
 PASSWD = os.environ['PASSWD']
 # TODO:not add webdav4 to requirements.txt yet
