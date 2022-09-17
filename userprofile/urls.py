e96e3261526e2454e785c40108e5318c203872ea
ksroido <ksxmyqj@gmail.com>
2022-09-17 16:27:30 +0800

    delete register

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # 用户登录
     path('login/', views.root_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
-    path('register/', views.user_register, name='register'),
+    # path('register/', views.user_register, name='register'),
     path('delete/', views.user_delete, name='delete'),
     path('manager/', views.user_delete, name='delete'),
 ]
